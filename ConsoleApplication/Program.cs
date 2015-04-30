c4a54981a621b4281b0e47f04b94a984d86f0c09
jarekz <jaroslaw.zajaczkowski@rightsoft.pl>
2015-04-30 07:34:43 +0200

    init

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using net.sf.jni4net;
+using net.sf.jni4net.jni;
+using pl.yaras.java;
+
+namespace ConsoleApplication
+{
+    public class Program : IDisposable
+    {
+        private JNIEnv env;
+
+        public Program()
+        {
+            var bridgeSetup = new BridgeSetup();
+            bridgeSetup.AddAllJarsClassPath(".");
+            this.env = Bridge.CreateJVM(bridgeSetup);
+            Bridge.RegisterAssembly(typeof(Calc).Assembly);
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.WriteLine(p.Add(2, 3));
+            Console.ReadKey();
+        }
+
+        public int Add(int a, int b)
+        {
+            return Calc.add(a, b);
+        }
+
+        public int Sub(int a, int b)
+        {
+            return Calc.sub(a, b);
+        }        
+
+        public void Dispose()
+        {
+            //this.env.GetJavaVM().DestroyJavaVM();
+        }
+    }
+}
---
+++ Jni4NetTestProject/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jni4NetTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Program p = new Program();
+            //using (Program p = new Program())
+            { 
+                Assert.AreEqual(5, p.Add(2, 3));
+            }
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Program p = new Program();
+            //using ()
+            {
+                Assert.AreEqual(7, p.Sub(10, 3));
+            }
+        }
+    }
+}
